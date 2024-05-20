6ff4d254456475256f25ab1f5b501d72d6f8cc46
DariaPrindina <megadaria2015@yandex.ru>
2024-05-20 21:58:43 +0300

    v flag

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -17,7 +17,7 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: 'git@github.com:DariaPrindina/web-plus-pm2-deploy.git',
       path: DEPLOY_PATH,
-      'pre-deploy': `scp -Cr .env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend && scp -Cr .env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend`,
+      'pre-deploy': `scp -Crv .env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend && scp -Crv .env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend`,
       'post-deploy': `cd ${DEPLOY_PATH}/source/backend && npm i && npm run build && pm2 restart ecosystem.config.js`,
     },
   },
