4b29f3606d748be1523601b6f545341c143b540c
DariaPrindina <megadaria2015@yandex.ru>
2024-05-20 21:09:02 +0300

    fix: ecosystem

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -4,19 +4,21 @@ const {
   DEPLOY_USER, DEPLOY_HOST, DEPLOY_REF, DEPLOY_PATH,
 } = process.env;
 
-export const apps = [{
-  name: 'mesto-backend',
-  script: './build/app.js',
-}];
+module.exports = {
+  apps: [{
+    name: 'mesto-backend',
+    script: './build/app.js',
+  }],
 
-export const deploy = {
-  production: {
-    user: DEPLOY_USER,
-    host: DEPLOY_HOST,
-    ref: DEPLOY_REF,
-    repo: 'https://github.com/DariaPrindina/web-plus-pm2-deploy.git',
-    path: DEPLOY_PATH,
-    'pre-deploy': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/backend`,
-    'post-deploy': `cd ${DEPLOY_PATH}/backend && npm i && npm run build && pm2 restart ecosystem.config.js`,
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: 'git@github.com:DariaPrindina/web-plus-pm2-deploy.git',
+      path: DEPLOY_PATH,
+      'pre-deploy': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend`,
+      'post-deploy': `cd ${DEPLOY_PATH}/backend && npm i && npm run build && pm2 restart ecosystem.config.js`,
+    },
   },
 };
