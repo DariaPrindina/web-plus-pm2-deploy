47b520a64f5167cf982c52544b9bce0827dbb2e1
DariaPrindina <megadaria2015@yandex.ru>
2024-05-20 21:30:11 +0300

    ecosystem front post deploy

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -4,18 +4,20 @@ const {
   DEPLOY_USER, DEPLOY_HOST, DEPLOY_REF, DEPLOY_PATH,
 } = process.env;
 
-export const apps = [{
-  name: 'mesto-frontend',
-  script: './build/app.js',
-}];
+module.exports = {
+  apps: [{
+    name: 'mesto-frontend',
+    script: './build/app.js',
+  }],
 
-export const deploy = {
-  production: {
-    user: DEPLOY_USER,
-    host: DEPLOY_HOST,
-    ref: DEPLOY_REF,
-    repo: 'https://github.com/DariaPrindina/web-plus-pm2-deploy.git',
-    path: DEPLOY_PATH,
-    'post-deploy': `cd ${DEPLOY_PATH}/source/frontend && npm i && npm run build && pm2 restart ecosystem.config.js `,
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: 'https://github.com/DariaPrindina/web-plus-pm2-deploy.git',
+      path: DEPLOY_PATH,
+      'post-deploy': `cd ${DEPLOY_PATH}/source/frontend && npm i && npm run build && pm2 restart ecosystem.config.js `,
+    },
   },
 };
