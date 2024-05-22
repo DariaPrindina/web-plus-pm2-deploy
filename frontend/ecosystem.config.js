21321b8a377086c7c089a0e7af14b0a94f312b75
Daria Prindina <megadaria2015@yandex.ru>
2024-05-22 23:14:11 +0300

    fff2

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -12,7 +12,7 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
-      'post-deploy': `cd ${DEPLOY_PATH}/source/frontend && npm install && npm run build && pm2 restart ecosystem.config.js `,
+      'post-deploy': `export PATH=$PATH:/home/megadaria2015/.nvm/versions/node/v21.7.3/bin && cd ${DEPLOY_PATH}/source/frontend && npm install && npm run build && pm2 restart ecosystem.config.js `,
     },
   },
 };
